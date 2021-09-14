06eb6ea39cb00d7673aa621b2f69068099c52761
beyondsun <suneethsunkari@gmail.com>
2021-09-14 11:22:55 +0530

    Sitecore Campaign ID List manager export

---
+++ Feature/code/ContactDataReaders/CampaignContactDataReader.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Diagnostics;
+using Sitecore.EmailCampaign.Model.XConnect.Events;
+using Sitecore.ListManagement.XConnect.Web.Export;
+using Sitecore.XConnect;
+using Sitecore.XConnect.Client;
+using Sitecore.XConnect.Client.Configuration;
+using Feature.Listmanager.xConnectModels;
+
+namespace Feature.Listmanager.ContactDataReaders
+{
+    public class CampaignContactDataReader : IContactDataReader
+    {
+        public string FacetName => ContactCustomFieldsFacet.DefaultFacetKey;
+
+        /// <summary></summary>
+        public string Map(Contact contact)
+        {
+            using (var client = SitecoreXConnectClientConfiguration.GetClient())
+            {
+                var result = "";
+                Interaction interaction = null;
+
+                try
+                {
+                    // retrieves all Email Sent Events for contact - desc order
+                    var queryable = client.Interactions.Where(x => x.Contact.Id == contact.Id
+                                                                   && x.Events.OfType<EmailSentEvent>()
+                                                                       .Any())
+                        .OrderByDescending(x => x.StartDateTime);
+
+                    var enumerable = queryable.GetBatchEnumeratorSync(20);
+
+                    // iterate until finds latest interaction on contact
+                    while (enumerable.MoveNext())
+                    {
+                        var interactionBatch = enumerable.Current; // Batch of <= 20 interactions
+
+                        interaction = interactionBatch.First(x => x.Events?.FirstOrDefault() != null);
+
+                        if (interaction?.CampaignId == null)
+                        {
+                            continue;
+                        }
+
+                        // retrieves campaign display name
+                        result = Factory.GetDatabase("master")
+                                     .GetItem(new ID(interaction.CampaignId.Value))
+                                     .DisplayName;
+
+                        break;
+                    }
+                }
+                catch (XdbExecutionException ex)
+                {
+                    Log.Error(ex.Message,
+                        ex,
+                        this);
+                }
+
+                return result;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# sitecore-listexporter
-Allows you export the last known campaign ID of a contact in a list.
+﻿# sitecore-campaignlistexporter
+Allows you to export the last known campaign ID of a contact in a list.
+
+![Trigerfish Logo](https://avatars.githubusercontent.com/u/78132139?v=4)
+
+## Compatibility
+- Tested in Sitecore 10
+
+## Prerequisites
+- Working sc 10 instance
+- xConnect service
+- Custom Facets for sc Contacts
+
+##Usage
+### Note
+The source code provides an example code on how to export the last known campaign ID of a contact in ListManager export.
+Copy the Feature/code to your working sitecore repo if you are following Helix structure (please refer to the File structure below on details)
+you need a proper working Sitecore 10 instance along with xConnect in order to test this E2E which is beyond the scope of this repo.
+
+### File Structure
+``` bash
+
+Feature/
+└── code/
+    └── ContactDataReaders
+        ├── CampaignContactDataReader.cs  //class implementing IContactDataReader
+    └── xConnectModels
+        ├── ContactCustomFieldsFacet.cs   //custom Facet class for example
+    └── App_config
+        ├── Feature.ListManager.config    // Patch for OOTB App_Config/Sitecore/ListManagement/Sitecore.ListManagement.config
+```
+
+### sitecore-campaignlistexporter
+- Sitecore ListManagement API provides a way to export the custom fields to the List Manager export contacts to CSV feature.
+- The code here makes use of the Sitecore ListManagement API in order to export the custom fields to the CSV using List Manager export contacts to CSV feature.
+- The patch file consists of a class implementeing IContactDataReader which grabs the campaign Id from the contact 
+& a field name which is column name on the export CSV.
+
+### CSV example
+[xls.png](https://postimg.cc/MMYPtcR2)
+
+## Dependenent packages sc 10.0.0
+- Sitecore.Kernel
+- Sitecore.xConnect
+- Sitecore.XConnect.Client
+- Sitecore.XConnect.Client.Configuration
+- Sitecore.EmailCampaign.Model
+- Sitecore.ListManagement.XConnect
+- Sitecore.ListManagement.XConnect.Web
+
+##Good reference here on Listmanager fields export
+![Link](https://sitecorify.com/list-manager-export-contacts-with-custom-facets/)
+
